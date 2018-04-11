333929481d4b5035c4f847722f89e36f2d8d0e84
John Raber <jrab224@gmail.com>
2018-04-11 01:05:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function sayHiToGrandma(string) {
   if string === "I love you, Grandma." {
     return "I love you, too."
   }  else if string === lowercase {
-    return "I can't hear you!"
+       return "I can't hear you!"
   }  else if string === uppercase {
-    return "YES INDEED!"
+       return "YES INDEED!"
   }
 }
